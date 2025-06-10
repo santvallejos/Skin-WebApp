50afee4fe6712d6da3327cc3a44101f20d1a5c99
santvallejos <vallejossantiago1412@gmail.com>
2025-06-10 11:27:04 -0300

    Installation of zustand

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,15 +1,15 @@
 import { useParams } from 'react-router-dom';
 
 function Product() {
-  const { id } = useParams();
+    const { id } = useParams();
 
-  return (
-    <>
-      <section>
-        <h2>Estas en el producto {id}</h2>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section>
+                <h2>Estas en el producto {id}</h2>
+            </section>
+        </>
+    );
 }
 
 export default Product;
